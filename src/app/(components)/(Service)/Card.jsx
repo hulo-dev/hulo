238cd48d6ac70fbfdddfbd50727b85f7a10fae8f
BryzulychV <bryzulych@intelliboard.net>
2023-06-07 12:14:21 +0300

    style section services

--- src/app/(components)/(Service)/Card.jsx
+++ src/app/(components)/(Service)/Card.jsx
@@ -5,20 +5,22 @@ const Card = ({ data }) => {
     return (
         <div className="card">
             <p className="card__title">{data.title}</p>
-            <div className="card__dody">
-                <p className="card__text">{data.text}</p>
-                <div className="gfhf">
-                    <Button link="#" name="Start a project" type="text" />
-                    <span className="border"></span>
-                    <div className="card__portfolio">
-                        <div className="card__portfolio_text">
-                            <TextWithSup text="TIMELINE 4-6" sup="WEEKS" />
-                            <TextWithSup text="CASES" sup="65" />
-                        </div>
-                        <div className="card__portfolio_img">
-                            <Image height={50} width={50} src="/b1.png" />
-                            <Image height={50} width={50} src="/b2.png" />
-                            <Image height={50} width={50} src="/b3.png" />
+            <div className="card__body_wrapper">
+                <div className="card__body">
+                    <p className="card__text">{data.text}</p>
+                    <div className="gfhf">
+                        <Button link="#" name="Start a project" type="text" />
+                        <span className="border"></span>
+                        <div className="card__portfolio">
+                            <div className="card__portfolio_text">
+                                <TextWithSup text="TIMELINE 4-6" sup="WEEKS" />
+                                <TextWithSup text="CASES" sup="65" />
+                            </div>
+                            <div className="card__portfolio_img">
+                                <Image height={50} width={50} src="/b1.png" />
+                                <Image height={50} width={50} src="/b2.png" />
+                                <Image height={50} width={50} src="/b3.png" />
+                            </div>
                         </div>
                     </div>
                 </div>

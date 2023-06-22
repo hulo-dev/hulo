20d664b0949905dafb51297771e29d049a8a0627
Volodymyr <bryzulych@intelliboard.net>
2023-06-22 08:47:27 +0300

    config for github pages

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const nextConfig = {
       includePaths: [path.join(__dirname, 'styles')],
     },
     // output: 'export',
-    images: { unoptimized: true },
+    images: { 
+      loader: 'akamai',
+      path: '',
+    },
     env:{
       REACT_APP_CONVERTKIT_API_KEY: 'vq_IA5HkNGRzDNeSnOmWyA',
       REACT_APP_CONVERTKIT_FORM_ID: 2923707,

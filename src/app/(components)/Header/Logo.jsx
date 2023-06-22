44164cf2bc17a25b394420e9b44e8514a4fcd495
Volodymyr <bryzulych@intelliboard.net>
2023-06-22 11:27:09 +0300

    fix home link

--- src/app/(components)/Header/Logo.jsx
+++ src/app/(components)/Header/Logo.jsx
@@ -2,17 +2,16 @@ import Link from "next/link";
 import { LogoSvg } from "../Icons";
 
 export const Logo = ({ setIsOpen, lock }) => {
-
     const clickLogoHandler = () => {
         setIsOpen(false);
         lock(false);
-    }
+    };
 
     return (
         <div className="logo">
-            <Link  onClick={clickLogoHandler} href="#">
+            <Link onClick={clickLogoHandler} href="/">
                 <LogoSvg />
             </Link>
         </div>
     );
-}
+};

d85c55434333329e7b9bf896336452bd9afa740e
Alen-Rouse <alenrouse123@gmail.com>
2024-01-22 13:53:41 +0200

    remove const array section gallary

--- src/app/(components)/(ProjectItem)/SectionGallary.jsx
+++ src/app/(components)/(ProjectItem)/SectionGallary.jsx
@@ -3,7 +3,6 @@ import { GridImages } from "./GridImages";
 import SectionText from "./SectionText";
 
 const SectionGallary = ({ data, arr }) => {
-  const array = [{src:`${data.img1.url}`},{src:`${data.img1.url}`},{src:`${data.img1.url}`}];
   return (
     <div>
       <DoubleText data={data}/> 

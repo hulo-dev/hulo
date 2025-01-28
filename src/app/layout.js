41d5425251491c19133a601f243d0de30e185016
bobUzh <bottopsz@gmail.com>
2025-01-28 13:23:09 +0200

    rollback

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,17 +30,15 @@ export default function RootLayout({ children }) {
             })(window, document, "clarity", "script", "pxbxayxiit");
           `}
         </Script>
-
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LG9TBV688X"></Script>
-      <Script>
-        {`            
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-LG9TBV688X');
-        `}
-      </Script>
+        <Script id="microsoft-clarity-analytics" strategy="beforeInteractive">
+          {`            
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NTPFKWDZ');
+          `}
+        </Script>
       </head>
       <body className={isLock ? 'lock' : ''}>
         <div className='overlay'/>
